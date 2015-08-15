ba3c24dc1f404597e8dd6bd6fe66a20421f12f43
MattNahirny <mnahirny1@my.bcit.ca>
2015-08-15 01:13:28 -0700

    Can completely view venalse

--- Script.js
+++ Script.js
@@ -1,131 +1,181 @@
-window.onload = init;
-var inView;
-var slotArray;
-var helminfo
-function init() {
-    slotArray = ["herohelm"];
-    helminfo = new Image();
-    helminfo.src = "./D2 Stream/LightalsE/helminfo.png"
 
+$(document).ready(function init() {
+    if ($("#inventory").length)
+        var invCtx = $("#inventory").get(0).getContext("2d");
 
+    //-------------
+    //All images go here
+    //-------------
+    var LightalsEinvimg = new Image();
+    LightalsEinvimg.src = "./D2 Stream/LightalsE/LightalsEInventory.png";
+    var VenalsEinvimg = new Image();
+    VenalsEinvimg.src = "./D2 Stream/VenalsE/VenalsEInventory.png"
+    var FalsmitEinvimg = new Image();
+    FalsmitEinvimg.src = "./D2 Stream/FalsmitE/FalsmitEInventory.png"
+    var lightalse = new Image();
+    lightalse.src = "./D2 Stream/LightalsE/LightalsE.gif";
+    var venalse = new Image();
+    venalse.src = "./D2 Stream/VenalsE/VenalsE.gif";
+    var falsmite = new Image();
+    falsmite.src = "./D2 Stream/FalsmitE/FalsmitE.gif";
 
-
-
-
-
-
-    var invCanvas = document.getElementById("inventory");
-    var invCtx = invCanvas.getContext("2d");
+    //VenalsE gear
+    var VenalsEhelm = new Image();
+    VenalsEhelm.src = "./D2 Stream/Venalse/GearInfo/herohelm.png";
+    var VenalsEammy = new Image();
+    VenalsEammy.src = "./D2 Stream/Venalse/GearInfo/heroammy.png";
+    var VenalsEwep1 = new Image();
+    VenalsEwep1.src = "./D2 Stream/Venalse/GearInfo/herowep1.png";
+    var VenalsEarmor = new Image();
+    VenalsEarmor.src = "./D2 Stream/Venalse/GearInfo/heroarmor.png";
+    var VenalsEwep2 = new Image();
+    VenalsEwep2.src = "./D2 Stream/Venalse/GearInfo/herowep2.png";
+    var VenalsEgloves = new Image();
+    VenalsEgloves.src = "./D2 Stream/Venalse/GearInfo/herogloves.png";
+    var VenalsEring1 = new Image();
+    VenalsEring1.src = "./D2 Stream/Venalse/GearInfo/heroring1.png";
+    var VenalsEbelt = new Image();
+    VenalsEbelt.src = "./D2 Stream/Venalse/GearInfo/herobelt.png";
+    var VenalsEring2 = new Image();
+    VenalsEring2.src = "./D2 Stream/Venalse/GearInfo/heroring2.png";
+    var VenalsEboots = new Image();
+    VenalsEboots.src = "./D2 Stream/Venalse/GearInfo/heroboots.png";
+    var VenalsEmercwep1 = new Image();
+    VenalsEmercwep1.src = "./D2 Stream/Venalse/GearInfo/mercwep1.png";
+    var VenalsEmerchelm = new Image();
+    VenalsEmerchelm.src = "./D2 Stream/Venalse/GearInfo/merchelm.png";
+    var VenalsEmercarmor = new Image();
+    VenalsEmercarmor.src = "./D2 Stream/Venalse/GearInfo/mercarmor.png";
+    var VenalsEcharm1 = new Image();
+    VenalsEcharm1.src = "./D2 Stream/Venalse/GearInfo/charm1.png";
+    var VenalsEcharm2 = new Image();
+    VenalsEcharm2.src = "./D2 Stream/Venalse/GearInfo/charm2.png";
+    var VenalsEcharm3 = new Image();
+    VenalsEcharm3.src = "./D2 Stream/Venalse/GearInfo/charm3.png";
+    var VenalsEcharm4 = new Image();
+    VenalsEcharm4.src = "./D2 Stream/Venalse/GearInfo/charm4.png";
+    var VenalsEcharm5 = new Image();
+    VenalsEcharm5.src = "./D2 Stream/Venalse/GearInfo/charm5.png";
+    var VenalsEcharm6 = new Image();
+    VenalsEcharm6.src = "./D2 Stream/Venalse/GearInfo/charm6.png";
 
     //Section for images at top
-    var c1 = document.getElementById("char1");
-    var ctx1 = c1.getContext("2d");
-    var lightalse = new Image();
-    inView = "none";
-    lightalse.onload = function () {
-        ctx1.drawImage(this, 0, 0);
-    };
-    c1.onselectstart = function () {
-        return false;
-    }
-    c1.onclick = function () {
-        inView = "LightalsE";
-        var inv = document.getElementById("inventory");
-        var invctx = inv.getContext("2d");
-        var invimg = new Image();
-        invimg.src = "./D2 Stream/LightalsE/LightalsEInventory.png"
-        invctx.drawImage(invimg, 0, 0);
-        setAllHovers(inView);
-    };
-    lightalse.src = "./D2 Stream/LightalsE/LightalsE.gif"
+    if ($("#char1").length)
+        var ctx1 = $("#char1").get(0).getContext("2d");
+    if ($("#char2").length)
+        var ctx2 = $("#char2").get(0).getContext("2d");
+    if ($("#char3").length)
+        var ctx3 = $("#char3").get(0).getContext("2d");
 
-    var c2 = document.getElementById("char2");
-    var ctx2 = c2.getContext("2d");
-    var venalse = new Image();
-    venalse.onload = function () {
+    $(window).load(function () {
+        ctx1.drawImage(lightalse, 0, 0);
+        ctx2.drawImage(venalse, 0, 0);
+        ctx3.drawImage(falsmite, 0, 0);
 
-        ctx2.drawImage(this, 0, 0);
-    };
-    c2.onselectstart = function () {
-        return false;
-    }
-    c2.onclick = function () {
-        inView = "VenalsE";
-        var inv = document.getElementById("inventory");
-        var invctx = inv.getContext("2d");
-        var invimg = new Image();
-        invimg.src = "./D2 Stream/VenalsE/VenalsEInventory.png"
-        invctx.drawImage(invimg, 0, 0);
-        setAllHovers(inView);
-    };
-    venalse.src = "./D2 Stream/VenalsE/VenalsE.gif"
 
-    var c3 = document.getElementById("char3");
-    var ctx3 = c3.getContext("2d");
-    var falsmite = new Image();
-    falsmite.onload = function () {
-        ctx3.drawImage(this, 0, 0);
-    };
-    c3.onselectstart = function () {
-        return false;
-    }
-    c3.onclick = function () {
-        inView = "FalsmitE";
-        var inv = document.getElementById("inventory");
-        var invctx = inv.getContext("2d");
-        var invimg = new Image();
-        invimg.src = "./D2 Stream/FalsmitE/FalsmitEInventory.png"
-        invctx.drawImage(invimg, 0, 0);
-        setAllHovers(inView);
-    };
-    falsmite.src = "./D2 Stream/FalsmitE/FalsmitE.gif"
+        $("#char1").click(function () {
+            $("#heroSelectText").text("LightalsE");
+            invCtx.drawImage(LightalsEinvimg, 0, 0);
+            setAllHovers(getWhichChar());
 
-    function setAllHovers(char) {
-        switch (char) {
-            case "LightalsE":
-                setHover(slotArray, helminfo, invCtx, invCanvas);
-                break;
-            case "VenalsE":
+        });
 
-                break;
-            case "FalsmitE":
+        $("#char2").click(function () {
+            $("#heroSelectText").text("VenalsE");
+            invCtx.drawImage(VenalsEinvimg, 0, 0);
+            setAllHovers(getWhichChar());
+        });
 
-                break;
 
+        $("#char3").click(function () {
+            $("#heroSelectText").text("FalsmitE");
+            invCtx.drawImage(FalsmitEinvimg, 0, 0);
+            setAllHovers(getWhichChar());
+        });
+
+
+        function setAllHovers(char) {
+            var slots;
+            switch (char) {
+                case "LightalsE":
+
+                    break;
+                case "VenalsE":
+                    slots = ["herohelm"];
+                    setHover(slots, VenalsEhelm, invCtx);
+                    slots = ["heroammy"];
+                    setHover(slots, VenalsEammy, invCtx);
+                    slots = ["herowep1"];
+                    setHover(slots, VenalsEwep1, invCtx);
+                    slots = ["heroarmor"];
+                    setHover(slots, VenalsEarmor, invCtx);
+                    slots = ["herowep2"];
+                    setHover(slots, VenalsEwep2, invCtx);
+                    slots = ["herogloves"];
+                    setHover(slots, VenalsEgloves, invCtx);
+                    slots = ["heroring1"];
+                    setHover(slots, VenalsEring1, invCtx);
+                    slots = ["herobelt"];
+                    setHover(slots, VenalsEbelt, invCtx);
+                    slots = ["heroring2"];
+                    setHover(slots, VenalsEring2, invCtx);
+                    slots = ["heroboots"];
+                    setHover(slots, VenalsEboots, invCtx);
+                    slots = ["inv8", "inv18", "inv28"];
+                    setHover(slots, VenalsEcharm1, invCtx);
+                    slots = ["inv9", "inv19", "inv29"];
+                    setHover(slots, VenalsEcharm2, invCtx);
+                    slots = ["inv10"];
+                    setHover(slots, VenalsEcharm3, invCtx);
+                    slots = ["inv20"];
+                    setHover(slots, VenalsEcharm4, invCtx);
+                    slots = ["inv30", "inv40"];
+                    setHover(slots, VenalsEcharm5, invCtx);
+                    slots = ["inv38"];
+                    setHover(slots, VenalsEcharm6, invCtx);
+                    break;
+                case "FalsmitE":
+
+                    break;
+
+            }
         }
-    }
 
-}
+    });
+
+});
 
 //--------------------------
 //Setting Hovers
 //--------------------------
+function getWhichChar() {
+    if ($("#heroSelectText").text() === "Select a hero")
+        return "none";
+    if ($("#heroSelectText").text() === "LightalsE")
+        return "LightalsE";
+    if ($("#heroSelectText").text() === "VenalsE")
+        return "VenalsE";
+    if ($("#heroSelectText").text() === "FalsmitE")
+        return "FalsmitE";
 
+    return;
+}
 
-function setHover(slots, image, ctx, canvas) {
+function setHover(slots, image, ctx) {
     for (var i = 0; i < slots.length; i++) {
-        var div = document.getElementById(slots[i]);
-        div.onmouseover = function () {
-            displayInfo(image, ctx, canvas);
-        };
-        div.onmouseout = function () {
-            removeInfo(ctx, canvas);
-        };
+        $("#" + slots[i]).mouseover(function () {
+            ctx.drawImage(image, $("#inventory").width()/4, 100);
+        });
+        $("#" + slots[i]).mouseout(function () {
+            removeInfo(ctx, getWhichChar());
+        });
     }
 };
 
-function displayInfo(image, ctx, canvas) {
-    ctx.drawImage(image, 100, 100);
-};
 
-function getView() {
-    return inView;
-}
-
-function removeInfo(ctx, canvas) {
-    var state = getView();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    switch (state) {
+function removeInfo(ctx, view) {
+    ctx.clearRect(0, 0, $("#inventory").width, $("#inventory").height);
+    switch (view) {
         case "LightalsE":
             var img = new Image();
             img.src = "./D2 Stream/LightalsE/LightalsEInventory.png";
