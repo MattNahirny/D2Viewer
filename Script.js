9cff1bc2cee28b03b56e1baf2d86b4d409aaebe5
MattNahirny <mnahirny1@my.bcit.ca>
2015-08-17 12:06:47 -0700

    lightalse up and running

--- Script.js
+++ Script.js
@@ -59,6 +59,69 @@ VenalsEmerchelm.src = "./D2 Stream/VenalsE/GearInfo/merchelm.png";
 var VenalsEmercarmor = new Image();
 VenalsEmercarmor.src = "./D2 Stream/VenalsE/GearInfo/mercarmor.png";
 
+//LightalsE gear
+var LightalsEhelm = new Image();
+LightalsEhelm.src = "./D2 Stream/LightalsE/GearInfo/herohelm.png";
+var LightalsEammy = new Image();
+LightalsEammy.src = "./D2 Stream/LightalsE/GearInfo/heroammy.png";
+var LightalsEwep1 = new Image();
+LightalsEwep1.src = "./D2 Stream/LightalsE/GearInfo/herowep1.png";
+var LightalsEarmor = new Image();
+LightalsEarmor.src = "./D2 Stream/LightalsE/GearInfo/heroarmor.png";
+var LightalsEwep2 = new Image();
+LightalsEwep2.src = "./D2 Stream/LightalsE/GearInfo/herowep2.png";
+var LightalsEgloves = new Image();
+LightalsEgloves.src = "./D2 Stream/LightalsE/GearInfo/herogloves.png";
+var LightalsEring1 = new Image();
+LightalsEring1.src = "./D2 Stream/LightalsE/GearInfo/heroring1.png";
+var LightalsEbelt = new Image();
+LightalsEbelt.src = "./D2 Stream/LightalsE/GearInfo/herobelt.png";
+var LightalsEring2 = new Image();
+LightalsEring2.src = "./D2 Stream/LightalsE/GearInfo/heroring2.png";
+var LightalsEboots = new Image();
+LightalsEboots.src = "./D2 Stream/LightalsE/GearInfo/heroboots.png";
+
+//Lightalse Charms
+var LightalsEcharm1 = new Image();
+LightalsEcharm1.src = "./D2 Stream/LightalsE/GearInfo/charm1.png";
+var LightalsEcharm2 = new Image();
+LightalsEcharm2.src = "./D2 Stream/LightalsE/GearInfo/charm2.png";
+var LightalsEcharm3 = new Image();
+LightalsEcharm3.src = "./D2 Stream/LightalsE/GearInfo/charm3.png";
+var LightalsEcharm4 = new Image();
+LightalsEcharm4.src = "./D2 Stream/LightalsE/GearInfo/charm4.png";
+var LightalsEcharm5 = new Image();
+LightalsEcharm5.src = "./D2 Stream/LightalsE/GearInfo/charm5.png";
+var LightalsEcharm6 = new Image();
+LightalsEcharm6.src = "./D2 Stream/LightalsE/GearInfo/charm6.png";
+var LightalsEcharm7 = new Image();
+LightalsEcharm7.src = "./D2 Stream/LightalsE/GearInfo/charm7.png";
+var LightalsEcharm8 = new Image();
+LightalsEcharm8.src = "./D2 Stream/LightalsE/GearInfo/charm8.png";
+var LightalsEcharm9 = new Image();
+LightalsEcharm9.src = "./D2 Stream/LightalsE/GearInfo/charm9.png";
+var LightalsEcharm10 = new Image();
+LightalsEcharm10.src = "./D2 Stream/LightalsE/GearInfo/charm10.png";
+var LightalsEcharm11 = new Image();
+LightalsEcharm11.src = "./D2 Stream/LightalsE/GearInfo/charm11.png";
+var LightalsEcharm12 = new Image();
+LightalsEcharm12.src = "./D2 Stream/LightalsE/GearInfo/charm12.png";
+var LightalsEcharm13 = new Image();
+LightalsEcharm13.src = "./D2 Stream/LightalsE/GearInfo/charm13.png";
+var LightalsEcharm14 = new Image();
+LightalsEcharm14.src = "./D2 Stream/LightalsE/GearInfo/charm14.png";
+var LightalsEcharm15 = new Image();
+LightalsEcharm15.src = "./D2 Stream/LightalsE/GearInfo/charm15.png";
+var LightalsEcharm16 = new Image();
+LightalsEcharm16.src = "./D2 Stream/LightalsE/GearInfo/charm16.png";
+//LightalsE Merc
+var LightalsEmercwep1 = new Image();
+LightalsEmercwep1.src = "./D2 Stream/LightalsE/GearInfo/mercwep1.png";
+var LightalsEmerchelm = new Image();
+LightalsEmerchelm.src = "./D2 Stream/LightalsE/GearInfo/merchelm.png";
+var LightalsEmercarmor = new Image();
+LightalsEmercarmor.src = "./D2 Stream/LightalsE/GearInfo/mercarmor.png";
+
 $(document).ready(function init() {
 
 
@@ -91,13 +154,15 @@ $(document).ready(function init() {
 
         $("#char2").click(function () {
             $("#heroSelectText").text("VenalsE");
+            clearCharInfo();
             invCtx.drawImage(VenalsEinvimg, 0, 0);
             setAllHovers(getWhichChar());
         });
 
 
         $("#char3").click(function () {
-            $("#heroSelectText").text("FalsmitE");
+            $("#heroSelectText").text("FalsmitE- unfinished");
+            clearCharInfo();
             invCtx.drawImage(FalsmitEinvimg, 0, 0);
             setAllHovers(getWhichChar());
         });
@@ -107,7 +172,64 @@ $(document).ready(function init() {
             var slots;
             switch (char) {
                 case "LightalsE":
-
+                    slots = ["herohelm"];
+                    setHover(slots, LightalsEhelm, invCtx);
+                    slots = ["heroammy"];
+                    setHover(slots, LightalsEammy, invCtx);
+                    slots = ["herowep1"];
+                    setHover(slots, LightalsEwep1, invCtx);
+                    slots = ["heroarmor"];
+                    setHover(slots, LightalsEarmor, invCtx);
+                    slots = ["herowep2"];
+                    setHover(slots, LightalsEwep2, invCtx);
+                    slots = ["herogloves"];
+                    setHover(slots, LightalsEgloves, invCtx);
+                    slots = ["heroring1"];
+                    setHover(slots, LightalsEring1, invCtx);
+                    slots = ["herobelt"];
+                    setHover(slots, LightalsEbelt, invCtx);
+                    slots = ["heroring2"];
+                    setHover(slots, LightalsEring2, invCtx);
+                    slots = ["heroboots"];
+                    setHover(slots, LightalsEboots, invCtx);
+                    slots = ["mercwep1"];
+                    setHover(slots, LightalsEmercwep1, invCtx);
+                    slots = ["mercarmor"];
+                    setHover(slots, LightalsEmercarmor, invCtx);
+                    slots = ["merchelm"];
+                    setHover(slots, LightalsEmerchelm, invCtx);
+                    slots = ["inv5"];
+                    setHover(slots, LightalsEcharm1, invCtx);
+                    slots = ["inv6"];
+                    setHover(slots, LightalsEcharm2, invCtx);
+                    slots = ["inv7"];
+                    setHover(slots, LightalsEcharm3, invCtx);
+                    slots = ["inv8"];
+                    setHover(slots, LightalsEcharm4, invCtx);
+                    slots = ["inv9"];
+                    setHover(slots, LightalsEcharm5, invCtx);
+                    slots = ["inv10"];
+                    setHover(slots, LightalsEcharm6, invCtx);
+                    slots = ["inv15"];
+                    setHover(slots, LightalsEcharm7, invCtx);
+                    slots = ["inv16"];
+                    setHover(slots, LightalsEcharm8, invCtx);
+                    slots = ["inv17"];
+                    setHover(slots, LightalsEcharm9, invCtx);
+                    slots = ["inv18", "inv28", "inv38"];
+                    setHover(slots, LightalsEcharm10, invCtx);
+                    slots = ["inv19", "inv29", "inv39"];
+                    setHover(slots, LightalsEcharm11, invCtx);
+                    slots = ["inv20", "inv30", "inv40"];
+                    setHover(slots, LightalsEcharm12, invCtx);
+                    slots = ["inv26"];
+                    setHover(slots, LightalsEcharm13, invCtx);
+                    slots = ["inv27"];
+                    setHover(slots, LightalsEcharm14, invCtx);
+                    slots = ["inv36"];
+                    setHover(slots, LightalsEcharm15, invCtx);
+                    slots = ["inv37"];
+                    setHover(slots, LightalsEcharm16, invCtx);
                     break;
                 case "VenalsE":
                     slots = ["herohelm"];
@@ -170,7 +292,7 @@ function getWhichChar() {
         return "LightalsE";
     if ($("#heroSelectText").text() === "VenalsE")
         return "VenalsE";
-    if ($("#heroSelectText").text() === "FalsmitE")
+    if ($("#heroSelectText").text() === "FalsmitE- unfinished")
         return "FalsmitE";
 
     return;
@@ -192,16 +314,16 @@ function setHover(slots, image, ctx) {
 function clearCharInfo() {
     var allslots = ["herohelm", "heroammy", "herowep1", "heroarmor",
      "herowep2", "herogloves", "heroring1", "herobelt", "heroring2",
-      "heroboots", "mercwep1", "mercarmor", "merchelm", 
-      "inv1", "inv2", "inv3", "inv4", "inv5", "inv6", "inv7", "inv8", 
-      "inv9", "inv10", "inv11", "inv12", "inv13", "inv14", "inv15", "inv16", 
-      "inv17", "inv18", "inv19", "inv20", "inv21", "inv22", "inv23", "inv24", 
-      "inv25", "inv26", "inv27", "inv28", "inv29", "inv30", "inv31", "inv32", 
+      "heroboots", "mercwep1", "mercarmor", "merchelm",
+      "inv1", "inv2", "inv3", "inv4", "inv5", "inv6", "inv7", "inv8",
+      "inv9", "inv10", "inv11", "inv12", "inv13", "inv14", "inv15", "inv16",
+      "inv17", "inv18", "inv19", "inv20", "inv21", "inv22", "inv23", "inv24",
+      "inv25", "inv26", "inv27", "inv28", "inv29", "inv30", "inv31", "inv32",
       "inv33", "inv34", "inv35", "inv36", "inv37", "inv38", "inv39", "inv40"];
 
-      for (var i = 0; i < allslots.length; i++) {
+    for (var i = 0; i < allslots.length; i++) {
         $("#" + allslots[i]).unbind("mouseover mouseout");
-      };
+    };
 }
 
 function removeInfo(ctx, view) {
@@ -226,6 +348,6 @@ function removeInfo(ctx, view) {
             break;
     }
 
-    
+
     ctx.restore();
 };
